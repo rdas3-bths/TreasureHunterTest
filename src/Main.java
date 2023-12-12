ef220133e79d96bc35af516032dd84171927104f
laptop <sample@bths.edu>
2023-12-12 14:49:15 -0500

    changed print

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         TreasureHunter t = new TreasureHunter("test");
         System.out.println(t);
-        System.out.println("adding more print statements!");
+        System.out.println("adding more print statements! LOLOOLOLOLOLOOOOL");
         System.out.println("yay treasure hunting is fun");
         System.out.println("one more print statement");
     }
