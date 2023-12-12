e9e9db17cea1364ed5562a6599bbfbe0783b232a
RDas3 <das@>
2023-12-12 14:21:48 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TreasureHunter t = new TreasureHunter("test");
+        System.out.println(t);
+    }
+}
---
+++ src/TreasureHunter.java
@@ -0,0 +1,11 @@
+public class TreasureHunter {
+    private String example;
+
+    public TreasureHunter(String e) {
+        example = e;
+    }
+
+    public String toString() {
+        return example;
+    }
+}
